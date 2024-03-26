5cd6eddf028d7933f5fe55c2d6969490bcb85e98
Sebastian <sgarciaguerrero@safareyes.es>
2024-03-26 13:57:45 +0100

    Componente IONIC Home + routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,16 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
+  // {
+  //   path: 'home',
+  //   loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  // },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {path: 'notium', component: HomeComponent},
+
 ];
