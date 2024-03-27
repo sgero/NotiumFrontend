efeb5f52577a23f53f9f1711e930f19e5913c414
Sebastian <sgarciaguerrero@safareyes.es>
2024-03-27 13:33:24 +0100

    Home + botones

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,21 @@ export class HomeComponent  implements OnInit {
 
   constructor() { }
 
+  onEnterButtonClickRest(): void {
+    // Lógica para lo que sucede al hacer clic en el botón "Enter"
+
+
+    console.log('Se hizo clic en el botón Enter');
+  }
+
+  onEnterButtonClickOcioN(): void {
+    // Lógica para lo que sucede al hacer clic en el botón "Enter"
+
+
+    console.log('Se hizo clic en el botón Enter');
+  }
+
+
   ngOnInit() {}
 
 }
