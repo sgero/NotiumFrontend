b7358bd0e89fe1f0d0a46dbfbcb70135dd1e514d
robeLocal <rbulloperez@safareyes.es>
2024-04-23 20:28:48 +0200

    RBP - 23/04/2024 - Inyección de httpclient

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import {provideHttpClient, withFetch} from "@angular/common/http";
 
 if (environment.production) {
   enableProdMode();
@@ -16,5 +17,6 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes),
+    provideHttpClient(withFetch()),
   ],
 });
