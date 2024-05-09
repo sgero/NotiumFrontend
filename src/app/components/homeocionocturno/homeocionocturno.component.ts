a9df581d1f1482a69a0212fff363e9972f7ac3a6
robeLocal <rbulloperez@safareyes.es>
2024-05-09 20:22:55 +0200

    RBP - 09/05/2024 - Actualizacion

--- src/app/components/homeocionocturno/homeocionocturno.component.ts
+++ src/app/components/homeocionocturno/homeocionocturno.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {FooterocionocturnoComponent} from "../footerocionocturno/footerocionocturno.component";
 import {HeaderocionocturnoComponent} from "../headerocionocturno/headerocionocturno.component";
-import {OcioNocturnoService} from "../../services/ocioNocturno.service";
+import {EventoService} from "../../services/evento.service";
 import {Evento} from "../../models/Evento";
-import {DatePipe, NgForOf} from "@angular/common";
-import {error} from "@angular/compiler-cli/src/transformers/util";
+import {DatePipe, NgForOf, NgIf} from "@angular/common";
 import {IonicModule} from "@ionic/angular";
 import {HeaderComponent} from "../header/header.component";
 import {FooterComponent} from "../footer/footer.component";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-homeocionocturno',
@@ -20,20 +20,25 @@ import {FooterComponent} from "../footer/footer.component";
     IonicModule,
     DatePipe,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NgIf,
+    FormsModule
   ],
   standalone: true
 })
 export class HomeocionocturnoComponent  implements OnInit {
 
   eventosActivos: Evento[] = [];
+  eventosEntreFechas: Evento[] = [];
+  esCliente?: boolean;
+  fecha: Date = new Date();
+  fechaSeleccionada?: Date ;
 
-  constructor(private ocioService : OcioNocturnoService) { }
+  constructor(private ocioService : EventoService) { }
 
   ngOnInit() {
     this.getEventos();
   }
-
   getEventos(){
     this.ocioService.getActivos().subscribe({
       next: value => {
@@ -43,6 +48,38 @@ export class HomeocionocturnoComponent  implements OnInit {
         console.error(e);
       }
     })
+  }
+
+  getEventosEntreFechas(fecha:Date){
+    let fechaInicio = this.convertirFechaAStringFormatoYYYYMMDD(fecha.toString());
+    let fechaFin = this.obtenerSiguienteDia(fechaInicio);
+    let fechaFinal = this.convertirFechaAStringFormatoYYYYMMDD(fechaFin.toISOString());
+    const params = {
+      fechaInicio: fechaInicio,
+      fechaFin: fechaFinal,
+    };
+    this.ocioService.entreDosFechas(params).subscribe({
+      next: value => {
+        this.eventosEntreFechas = value.object as Evento[];
+      },
+      error: e => {
+        console.error(e);
+      }
+    })
+  }
+
+  obtenerSiguienteDia(fecha: string): Date {
+    const fechaSiguiente = new Date(fecha);
+    fechaSiguiente.setDate(fechaSiguiente.getDate() + 1);
+
+    return fechaSiguiente;
+  }
+
+  convertirFechaAStringFormatoYYYYMMDD(fecha:string): string{
+    fecha = fecha.substring(0, 10);
+    fecha = fecha.replace(/-/g, '/');
+
+    return fecha;
   }
 
 }
--- src/app/services/evento.service.ts
+++ src/app/services/evento.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {CrearEvento} from "../models/CrearEvento";
 import {CrearEventoCiclico} from "../models/CrearEventoCiclico";
 import {RespuestaDTO} from "../models/RespuestaDTO";
@@ -8,7 +8,7 @@ import {RespuestaDTO} from "../models/RespuestaDTO";
   providedIn: 'root'
 })
 
-export class OcioNocturnoService{
+export class EventoService {
   private apiUrl = 'http://127.0.0.1:8080';
   constructor(private http: HttpClient) { }
   getAll(){
@@ -17,6 +17,23 @@ export class OcioNocturnoService{
 
   getActivos(){
     return this.http.get<RespuestaDTO>(`${this.apiUrl}/eventos/activos`);
+  }
+
+  entreDosFechas(params:any){
+    let httpParams = new HttpParams();
+    Object.keys(params).forEach(key => {
+      httpParams = httpParams.append(key, params[key]);
+    });
+    return this.http.get<RespuestaDTO>(`${this.apiUrl}/eventos/fechas`, {params:httpParams});
+  }
+
+  entreDosFechasConIdOcio(idOcio:number, fechaInicio:Date, fechaFin:Date){
+    const datos = {
+      idOcio: idOcio,
+      fechaInicio: fechaInicio,
+      fechaFin: fechaFin
+    };
+    return this.http.post<RespuestaDTO>(`${this.apiUrl}/eventos/fechasYOcio`, datos);
   }
 
   guardarEvento(eventoData: CrearEvento){
