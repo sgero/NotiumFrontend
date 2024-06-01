6755b97102f1b66937cb458488cfb963dc3a45f1
Sebas Garcia <sgarciaguerrero@safareyes.es>
2024-06-02 01:09:25 +0200

    SGG - 01/06/2024 - Implementacion de Email por frontend (fallo)

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,21 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import {DomSanitizer} from "@angular/platform-browser";
+import {EmailService} from "../../services/email.service";
+import {IonicModule, ToastController} from '@ionic/angular';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
+  imports: [
+    IonicModule
+  ],
   standalone: true
 })
 export class FooterComponent  implements OnInit {
 
   emailLink: any;
 
-  constructor(private sanitizer: DomSanitizer) {
+  constructor(private sanitizer: DomSanitizer,
+      private emailService: EmailService,
+              private toastController: ToastController) {
     const email = 'info@notium.com'; // Dirección de correo electrónico
     const mailtoLink = `mailto:${email}`;
     this.emailLink = this.sanitizer.bypassSecurityTrustUrl(mailtoLink);
   }
+
+
+  async sendEmail() {
+    this.emailService.sendEmail('notiumevents@gmail.com', 'Subject', 'Message body')
+      .subscribe({
+        next: async (response) => {
+          const toast = await this.toastController.create({
+            message: 'Correo enviado exitosamente',
+            duration: 2000,
+            position: 'top',
+            color: 'success'
+          });
+          toast.present();
+        },
+        error: async (error) => {
+          const toast = await this.toastController.create({
+            message: 'Error al enviar el correo',
+            duration: 2000,
+            position: 'top',
+            color: 'danger'
+          });
+          await toast.present();
+        }
+      });
+  }
+
+
   ngOnInit() {}
 
 }
